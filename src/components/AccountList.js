db53e12423642b906feffd9ce4b505361b06ddcc
MD <mdoremy@gmail.com>
2022-05-11 23:49:21 -0400

    Edited App and AccountList.js

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 class AccountList extends React.component {
 
     render() {
+        console.log(this.props);
         return (
             <div>Accounts</div>
         )
@@ -10,4 +12,10 @@ class AccountList extends React.component {
 
 }
 
-export default AccountList;
+const mapStateToProps = state => {
+    return {
+        accounts: state.accounts.accounts
+    }
+};
+
+export default connect(mapStateToProps)(AccountList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import {combineReducers} from "redux";
 
 const DEFAULT_STATE = {
-
+accounts: [
+    {id:1, name: 'Armin',},
+    {id:2, name: 'Jacob',},
+    {id:3, name: 'Helen',},
+    {id:3, name: 'Omar',},
+]
 };
 
 const accountReducer = (state = DEFAULT_STATE, action) => {

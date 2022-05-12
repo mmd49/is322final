426e5a8ea457aa96f508c366ce3278693c34f002
MD <mdoremy@gmail.com>
2022-05-11 23:53:14 -0400

    Quick change to test accounts in index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import {combineReducers} from "redux";
 
 const DEFAULT_STATE = {
 accounts: [
-    {id:1, name: 'Armin',},
-    {id:2, name: 'Jacob',},
-    {id:3, name: 'Helen',},
-    {id:3, name: 'Omar',},
+    {id:1, name: 'Armin', balance: 1189.00},
+    {id:2, name: 'Jacob', balance: 1289.00},
+    {id:3, name: 'Helen', balance: 1179.00},
+    {id:3, name: 'Omar', balance: 1181.00},
 ]
 };
 

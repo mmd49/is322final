aedff60b80c80e580e29ae9010753be13d24d47f
MD <mdoremy@gmail.com>
2022-05-11 23:30:08 -0400

    Edited App and AccountList.js

---
+++ src/components/AccountList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AccountList extends React.component {
+
+    render() {
+        return (
+            <div>Accounts</div>
+        )
+    }
+
+}
+
+export default AccountList;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import AccountList from "./AccountList";
+
 const App = () => {
     return (
         <div className="container" style={{ marginTop: '15px'}}>
             <div className="row">
                 <div className="col-sm-4">
-                    Accounts
+                    <AccountList/>
                 </div>
                 <div className="col-sm-4">
                     Transactions
